40fa2f6f00304bfda831e9b820597be979734091
Rivan Zhang <47588828+hcxr1@users.noreply.github.com>
2024-01-26 22:24:06 +0700

    Update webhook.py

--- webhook/webhook.py
+++ webhook/webhook.py
@@ -2,8 +2,8 @@ import requests
 from datetime import datetime
 
 class Webhook:
-  def __init__ (self, webhook_url, webhook_id, webhook_token):
-    self.url = webhook_url
+  def __init__ (self, webhook_id, webhook_token):
+    self.url = "https://discord.com/api/webhooks/"
     self.id = webhook_id
     self.token = webhook_token
     self.data = {}
@@ -14,41 +14,46 @@ class Webhook:
     #for all params, see https://discordapp.com/developers/docs/resources/webhook#execute-webhook
     self.data["username"] = "TvAlert Bot"
     self.data["allowed_mentions"] = { "parse": [ "everyone "]}
-    self.data["content"] = "@everyone Current Update: " + today
+    self.data["content"] = "@everyone **" + message["exchange"] + ":" + message["symbol"] + "** Update: " + today
 
     #for all params, see https://discordapp.com/developers/docs/resources/channel#embed-object
     self.data["embeds"] = []
+    embed_obj = {}
+    temp = ""
 
-    for item in message:
-      embed_obj = {}
-      embed_obj["title"] = item["exchange"] +":" + item["symbol"]
-      embed_obj["description"] = """
-      **SENTIMENT**: **{0}** [BUY: {1} | SELL: {2} | NEUTRAL: {3}]
-      
-      **OHLC** {4} {5} {6} {7}
-      **VOLUME** {8}
-      **RSI** {9} [*{10}*]
-      """.format(
+    if "summary" in message.keys():
+        temp += "**SENTIMENT**: **{0}** [BUY: {1} | SELL: {2} | NEUTRAL: {3}]\n".format(
           item['summary']['RECOMMENDATION'],
           item['summary']['BUY'],
           item['summary']['SELL'],
           item['summary']['NEUTRAL'],
-          item['statistics']['open'],
-          item['statistics']['high'],
-          item['statistics']['low'],
-          item['statistics']['close'],
-          item['rsi']['rsi'],
-          item['rsi']['status']
-          )
+            )
 
-      if item['summary']['RECOMMENDATION'] == "SELL":
+    if "statistics" in message.keys():
+        temp += "**OHLC** {0} {1} {2} {3}\n**VOLUME** {4}\n".format(
+                    item['statistics']['open'],
+                    item['statistics']['high'],
+                    item['statistics']['low'],
+                    item['statistics']['close'],
+                    item['statistics']['volume']
+            )
+
+    if "rsi" in message.keys():
+        temp += "**RSI** {0} [*{1}*]\n".format(
+                    item['rsi']['rsi'],
+                    item['rsi']['status']
+            )
+
+    embed_obj["description"] = temp
+
+    if item['summary']['RECOMMENDATION'] == "SELL":
         embed_obj["color"] = 16711680 #red
-      elif item['summary']['RECOMMENDATION'] == "BUY":
+    elif item['summary']['RECOMMENDATION'] == "BUY":
         embed_obj["color"] = 65280 #green
-      else: 
+    else:
         embed_obj["color"] = 16777215 #white
 
-      self.data["embeds"].push(embed_obj)
+    self.data["embeds"].push(embed_obj)
 
 
   def send_notification(self):
